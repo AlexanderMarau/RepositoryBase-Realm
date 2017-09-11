ce23d4fd8a0ed4ea38f2653f4b33b7e605f0346f
Adriano Candido da Silva <acslook@hotmail.com>
2017-09-10 21:53:28 -0300

    AutoGenerate Id

--- RealmExample/Models/Produto.cs
+++ RealmExample/Models/Produto.cs
@@ -6,6 +6,11 @@ namespace RealmExample.Models
 {
     public class Produto : RealmObject, IContractID
     {
+        public Produto()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
         [PrimaryKey]
         public string Id { get; set; }
         public string Descricao { get; set; }
